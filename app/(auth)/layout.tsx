3712fb4472f02a11a8894b0fae67bd618baa84ab
unknown <akindav16@gmail.com>
2024-10-16 11:21:34 +0100

    fixing auth styling components

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,10 @@
-import { ClerkProvider } from "@clerk/nextjs";
+import React from "react";
+import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import "../../styles/globals.css";
 
 const inter = Poppins({
   subsets: ["latin"],
@@ -7,7 +12,7 @@ const inter = Poppins({
 });
 
 export const metadata = {
-  title: "Tweet: Share your views and insights, Inspire others",
+  title: "Auth",
   description: "A Nextjs meta application for Tweets",
 };
 
@@ -15,13 +20,15 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}) {  
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
-        <body className={inter.className}>
-          <div className="w-full flex justify-center items-center min-h-screen">{children}</div>
-        </body>
+        <body className={`${inter.className} bg-dark-1 flex items-center justify-center min-h-screen`}>{children}</body>
       </html>
     </ClerkProvider>
   );
